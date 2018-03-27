0ecb353dbb477892259a779a13ca29e09660ae9b
Franklin Oder <fjoder@gmail.com>
2018-03-26 23:37:04 -0400

    Lesson 2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const headers = React.createElement(
-  'tr',
-  { },
-  React.createElement('th', {}, 'First Name'),
-  React.createElement('th', {}, 'Last Name')
-);
-const janet = React.createElement(
-  'tr',
-  { },
-  React.createElement('td', {}, 'Janet'),
-  React.createElement('td', {}, 'James')
-);
-const john = React.createElement(
-  'tr',
-  { },
-  React.createElement('td', {}, 'John'),
-  React.createElement('td', {}, 'Jameson')
+// Hands-On 1
+//------------
+const jsx = (
+  <div>
+    
+  </div>
 );
 
-ReactDOM.render(
-  React.createElement(
-    'table', 
-    {}, 
-    headers,
-    janet,
-    john),
-  document.getElementById('root')
-);
+ReactDOM.render(jsx, document.getElementById('root'));
---
+++ src/lessons/lesson2.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Hands-On 1
+//------------
+// const jsx = (
+//   <div>
+//     <h1>Tips for breaking down a new concept</h1>
+//     <div>
+//       <ul>
+//         <li>Many concepts can't be tackled all at once</li>
+//         <ul>
+//           <li>Work with the parts you can understand</li>
+//           <li>Take breaks to stay fresh</li>
+//         </ul>
+//         <li>Develop a plan to learn the parts you don't know</li>
+//         <ul>
+//           <li>Start with the parts that you know</li>
+//           <li>Build new concepts on top</li>
+//         </ul>
+//       </ul>
+//     </div>
+//   </div>
+// );
+
+// ReactDOM.render(jsx, document.getElementById('root'));
+
+
+// Hands-On 2
+//------------
+// const makeDescriptions = item =>
+//   (
+//     <p>{item.title}: {item.description}</p>
+//   );
+
+// const libraries = [
+//   {
+//     title: 'React',
+//     description: 'A JavaScript library for building user interfaces'
+//   },
+//   {
+//     title: 'Redux',
+//     description: 'A predictable state container for JavaScript apps'
+//   },
+//   {
+//     title: 'Reselect',
+//     description: 'A simple selector library for Redux'
+//   }
+// ];
+// const output = libraries.map(makeDescriptions);
+
+// ReactDOM.render(<div>{output}</div>, document.getElementById('root'));

dcbd8423f8d39cf895ad46a1df223c40c2610fba
Franklin Oder <fjoder@gmail.com>
2018-04-02 00:02:13 -0400

    Lesson 8 update Mine component

--- src/screens/Mine.js
+++ src/screens/Mine.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Header from "../components/Header";
+import { saveRepos } from "../actions/AppActions";
 
-const Mine = () => <Header title="Mine" />;
+class Mine extends React.Component {
+  state = {
+  };
 
-export default Mine;
+  getMyRepos = () => {
+    fetch("https://api.github.com/users/fjoder/repos")
+      .then(response => response.json())
+      .then(respJson => {
+        this.props.dispatch(saveRepos(respJson.items));
+      });
+  };
+
+  createListItems = () => {
+    this.props.repos.map((r, index) => {
+      <li key={index}><a href={r.html_url}>{r.name}</a></li>
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Header title="Mine" />
+        <hr />
+        <button
+        onClick={this.getMyRepos()}>Load Repos
+        </button>
+        <ul>
+          {this.createListItems()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    repos: state.repos
+  };
+};
+
+export default connect(mapStateToProps)(Mine);

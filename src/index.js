91bd9febd35839b077574e3a901af2eebfc14c21
Franklin Oder <foder@etech-group.com>
2018-04-02 14:35:05 -0400

    Added store with reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createStore } from 'redux';
 import { Provider } from "react-redux";
 import { Home, Search, Mine } from "./screens";
 
-const Index = ({ store }) => (
+const initialState = {
+  repos: {}
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SAVE_REPOS':
+      return { ...state, repos: action.repos };
+    default:
+      return { ...state };
+  }
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const Index = (store) => (
   <Provider store={store}>
     <Router>
       <div>
